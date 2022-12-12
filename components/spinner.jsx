b6b7913f835fae7858724e21e71e841e2824d5f2
Vivek1998-G <vivekgawande8@gmail.com>
2022-12-12 17:21:35 +0530

    first commit

---
+++ components/spinner.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import React from 'react';
+import spinner from '../public/spinner.GIF';
+
+const Spinner = () => {
+  return (
+    <>
+      <Image className='w-[200px] m-auto block' src={spinner} alt='loading..' />
+    </>
+  );
+};
